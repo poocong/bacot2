52a05d9c0bc4482aecc61a432065668487a3b215
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2021-12-27 12:44:38 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/Stinkysupport untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/PrimeSupportGroup untuk Bantuan"
                 )
                 sys.exit()
         if FORCE_SUB_GROUP:
@@ -64,7 +64,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_GROUP dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Group Saat Ini: {FORCE_SUB_GROUP}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/Stinkysupport untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/PrimeSupportGroup untuk Bantuan"
                 )
                 sys.exit()
         try:
@@ -78,13 +78,13 @@ class Bot(Client):
                 f"Pastikan Bot adalah Admin di Channel DataBase, dan Periksa kembali Nilai CHANNEL_ID, Nilai Saat Ini: {CHANNEL_ID}"
             )
             self.LOGGER(__name__).info(
-                "\nBot Berhenti. Gabung Group https://t.me/Stinkysupport untuk Bantuan"
+                "\nBot Berhenti. Gabung Group https://t.me/PrimeSupportGroup untuk Bantuan"
             )
             sys.exit()
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BOT FILE SHARING BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/Stinkyrz"
+            f"[🔥 BOT FILE SHARING BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/Bukan_guudlooking"
         )
         self.username = usr_bot_me.username
 
