d0be41fdc2f93583241753b8c986a102fbf05a23
Destra1112 <97810837+Destra1112@users.noreply.github.com>
2022-01-16 06:00:03 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ from config import (
     APP_ID,
     CHANNEL_ID,
     FORCE_SUB_CHANNEL,
+    FORCE_SUB_CHANNELL, 
     FORCE_SUB_GROUP,
     LOGGER,
     OWNER,
@@ -46,6 +47,22 @@ class Bot(Client):
                 )
                 self.LOGGER(__name__).warning(
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
+                )
+                self.LOGGER(__name__).info(
+                    "\nBot Berhenti. Silahkan bertanya ke @DessXtrash untuk Bantuan"
+                )
+                sys.exit()
+        if FORCE_SUB_CHANNELL:
+            try:
+                link = await self.export_chat_invite_link(FORCE_SUB_CHANNELL)
+                self.invitelink = link
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning(
+                    "Bot tidak dapat Mengambil link Undangan dari FORCE_SUB_CHANNELL!"
+                )
+                self.LOGGER(__name__).warning(
+                    f"Silakan periksa kembali var FORCE_SUB_CHANNELL dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNELL}"
                 )
                 self.LOGGER(__name__).info(
                     "\nBot Berhenti. Silahkan bertanya ke @DessXtrash untuk Bantuan"
